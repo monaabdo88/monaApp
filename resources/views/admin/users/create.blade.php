4f4db56cefcd43782c60a150dc6574ffd3ad29b0
monaabdo88 <monaabdo88@gmail.com>
2018-01-13 16:04:02 +0200

    create photo for users

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -24,8 +24,8 @@
         {!! Form::select('is_active',array('1'=>'Active','0'=>'Not Active'),null,['class'=>'form-control']) !!}
     </div>
     <div class="form-group">
-        {!! Form::label('file','Photo') !!}
-        {!! Form::file('file',null,['class'=>'form-control']) !!}
+        {!! Form::label('photo_id','Photo') !!}
+        {!! Form::file('photo_id',null,['class'=>'form-control']) !!}
     </div>
     <div class="form-group">
         {!! Form::submit('Add User',['class'=>'btn btn-success']) !!}
